9054a273c5e679e5436300209c74bb9786165118
Mun-101 <151182410+Mun-101@users.noreply.github.com>
2025-11-23 15:23:27 -0400

    Add placeholder page

--- login/static/login/index.js
+++ login/static/login/index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:login/static/login/index.js
 document.getElementById("loginForm").addEventListener("submit", function(e) {
   e.preventDefault();
 
@@ -11,3 +12,18 @@ document.getElementById("loginForm").addEventListener("submit", function(e) {
     alert("Invalid credentials. Try username: student, password: acadia");
   }
 });
+=======
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (username === "student" && password === "acadia") {
+    alert("Login successful! Redirecting to chat...");
+    window.location.href = "chat.html"; // future chat page
+  } else {
+    alert("Invalid credentials. Try username: student user ID, password: ");
+  }
+});
+>>>>>>> Stashed changes:index.js
